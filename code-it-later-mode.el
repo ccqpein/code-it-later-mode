a45254327f63e7701a44c1af839b7b8606c9c68e
ccQpein <ccqpein@protonmail.com>
2023-10-13 22:30:09 -0400

    add doc

--- code-it-later-mode.el
+++ code-it-later-mode.el
@@ -11,7 +11,7 @@
 
 ;;; Commentary:
 
-;; This is the interactive mode for emacs with [codeitlater](https://github.com/ccqpein/code-it-later-rs)
+;; This is the interactive mode for Emacs with [codeitlater](https://github.com/ccqpein/code-it-later-rs)
 
 ;;; Code:
 
@@ -19,42 +19,44 @@
 (require 'helm)
 
 (defgroup code-it-later nil
-  "code-it-later emacs mode"
+  "Code-it-later Emacs mode."
   :group 'helm)
 
 (defcustom code-it-later-keywords nil
-  "the keywords options for code-it-later -k/--keywords"
+  "The keywords options for `code-it-later' -k/--keywords."
   :type '(repeat string)
   :group 'code-it-later)
 
 (defcustom code-it-later-filetypes nil
-  "the keywords options for code-it-later -f/--filetypes"
+  "The keywords options for `code-it-later' -f/--filetypes."
   :type '(repeat string)
   :group 'code-it-later)
 
 (defcustom code-it-later-ignore-dirs nil
-  "the keywords options for code-it-later -x/--ignore-dir"
+  "The keywords options for `code-it-later' -x/--ignore-dir."
   :type '(repeat string)
   :group 'code-it-later)
 
 (defcustom code-it-later-config-file-directory nil
-  "the options for code-it-later -C/--config"
+  "The options for `code-it-later' -C/--config."
   :type 'directory
   :group 'code-it-later)
 
 (defcustom code-it-later-show-ignored nil
-  "the options for code-it-later --show-ignored"
+  "The options for `code-it-later' --show-ignored."
   :type 'boolean
   :group 'code-it-later)
 
-(defvar code-it-later-version nil "code-it-later version")
+(defvar code-it-later-version nil "Code-it-later version.")
 
 (defun code-it-later--version ()
-  "split and get the version of code-it-later"
+  "Split and get the version of `code-it-later'."
   (string-to-number (cadr (split-string (shell-command-to-string "codeitlater -V") " "))))
 
 ;;:= TODO: keywords should give some color??
 (defun code-it-later--filter-one-by-one (candidate)
+  "Async filter one by one function.
+Argument CANDIDATE."
   (let* ((split (helm-grep-split-line candidate))
 		 (file (nth 0 split))
          (lineno (nth 1 split))
@@ -67,7 +69,8 @@
           candidate)))
 
 (defun code-it-later--persistent-action (candidate)
-  "persistent-action of helm-async source"
+  "Persistent-action of helm-async source.
+Argument CANDIDATE."
   (let* ((file-line (helm-grep-split-line candidate))
 		 (filename (or (cl-first file-line) candidate))
 		 (line (cl-second file-line)))
@@ -79,10 +82,14 @@
 	))
 
 (defun code-it-later--action-find-file (candidate)
+  "Not documented.
+Argument CANDIDATE ."
   (code-it-later--persistent-action candidate)
   )
 
 (defun code-it-later--action-find-file-other-window (candidate)
+  "Not documented.
+Argument CANDIDATE ."
   (let* ((file-line (helm-grep-split-line candidate))
 		 (filename (or (cl-first file-line) candidate))
 		 (line (cl-second file-line)))
@@ -93,6 +100,7 @@
 	(helm-highlight-current-line)))
 
 (defvar code-it-later--actions
+  "Not documented."
   (helm-make-actions
    "Open file"              #'code-it-later--action-find-file
    "Open file other window" #'code-it-later--action-find-file-other-window))
@@ -103,9 +111,10 @@
    (persistent-action :initform 'code-it-later--persistent-action)
    (action :initform 'code-it-later--actions)
    )
-  "async helm source for code-it-later")
+  "Async helm source for code-it-later.")
 
 (cl-defun code-it-later--make-arguments (&key keywords filetypes ignore-dirs config-file-directory show-ignored)
+  "Not documented."
   (let ((arguments ""))
 	(when (and config-file-directory (>= code-it-later-version 0.7))
 	  (setf arguments
@@ -148,7 +157,7 @@
 	))
 
 (cl-defun code-it-later--make-command (dirs &optional keywords filetypes ignore-dirs config-file-directory show-ignored)
-  "make codeitlater command"
+  "Make codeitlater command."
   (let ((comm "codeitlater -O list ")
 		(argumests (code-it-later--make-arguments :keywords keywords
 								   :filetypes filetypes
@@ -162,7 +171,7 @@
 	))
 
 (defun code-it-later--do (dirs keywords filetypes ignore-dirs config-file-directory show-ignored)
-  "do the codeitlater as the shell command"
+  "Do the codeitlater as the shell command."
   (let* ((comm (code-it-later--make-command dirs
 											keywords
 											filetypes
@@ -184,6 +193,7 @@
 					  )))))))))
 
 (defun code-it-later--string-join (ss &optional join-str)
+  "String join helper function."
   (let ((j (if join-str join-str " ")))
 	(cl-loop with result = (car ss)
 			 for s in (cdr ss)
@@ -193,10 +203,10 @@
 
 
 (defvar code-it-later-source nil
-  "the helm source of code-it-later")
+  "The helm source of code-it-later.")
 
 (defun code-it-later--set-source (dirs keywords filetypes ignore-dirs config-file-directory show-ignored)
-  "set source"
+  "Set source."
   (setf code-it-later-source
 		(helm-make-source "code-it-later"
 			'code-it-later-class
@@ -216,26 +226,27 @@
 		  )))
 
 (defun code-it-later--prompt-keywords ()
-  "prompt for keywords"
+  "Prompt for keywords."
   (split-string (read-string "input the keyword(s): ") "[ |, *]+" t))
 
 (defun code-it-later--prompt-filetypes ()
-  "prompt for filetypes"
+  "Prompt for filetypes."
   (split-string (read-string "input the filetype(s): ") "[ |, *]+" t))
 
 (defun code-it-later--prompt-ignore-dirs ()
-  "prompt for ignore dirs"
+  "Prompt for ignore dirs."
   (split-string (read-string "input the ignore-dir(s): ") "[ |, *]+" t))
 
 (defun code-it-later--prompt-config-file-directory ()
-  "prompt for config-file-directory"
+  "Prompt for config-file-directory."
   (read-string "input the config file directory: "))
 
 (defun code-it-later--prompt-show-ignored ()
-  "prompt for if show-ignored"
+  "Prompt for if show-ignored."
   (yes-or-no-p "do you want to show ignore crumbs?"))
 
 (cl-defun code-it-later--prompt ()
+  "Prompt for code-it-later."
   (let ((keywords code-it-later-keywords)
 		(filetypes code-it-later-filetypes)
 		(ignore-dirs code-it-later-ignore-dirs)
@@ -271,6 +282,7 @@
 
 ;;;###autoload
 (defun code-it-later (&optional arg)
+  "major function"
   (interactive "P")
   (if (not code-it-later-version)
 	  (setf code-it-later-version (code-it-later--version)))
